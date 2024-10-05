ec0a063c9087bad752189ba0520747f696d79033
Leon David Zipp <leondavidzipp@gmx.de>
2024-10-05 14:25:25 +0200

    formatting

--- packages/foundry/script/Deploy.s.sol
+++ packages/foundry/script/Deploy.s.sol
@@ -5,12 +5,12 @@ import "./DeployHelpers.s.sol";
 import { DeployYourContract } from "./DeployYourContract.s.sol";
 
 contract DeployScript is ScaffoldETHDeploy {
-  function run() external {
-    DeployYourContract deployYourContract = new DeployYourContract();
-    deployYourContract.run();
+    function run() external {
+        DeployYourContract deployYourContract = new DeployYourContract();
+        deployYourContract.run();
 
-    // deploy more contracts here
-    // DeployMyContract deployMyContract = new DeployMyContract();
-    // deployMyContract.run();
-  }
+        // deploy more contracts here
+        // DeployMyContract deployMyContract = new DeployMyContract();
+        // deployMyContract.run();
+    }
 }
