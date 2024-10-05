648abc25a5d4c0efe1d7423a694da052b7625b51
Leon David Zipp <leondavidzipp@gmx.de>
2024-10-05 20:50:44 +0200

    added mint methods

--- packages/foundry/contracts/ApprovalNFTV2.sol
+++ packages/foundry/contracts/ApprovalNFTV2.sol
@@ -2,14 +2,15 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/utils/Nonces.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "permit2/src/interfaces/IAllowanceTransfer.sol";
 
-contract ApprovalNFT is ERC721, IERC721Receiver, Ownable, Nonces, ReentrancyGuard {
-    mapping(uint256 => IAllowanceTransfer.PermitBatch) internal permits;
+contract ApprovalNFT is ERC721Enumerable, IERC721Receiver, Ownable, Nonces, ReentrancyGuard {
+    IAllowanceTransfer private constant _PERMIT_2 = IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+    mapping(uint256 tokenId => IAllowanceTransfer.PermitDetails[]) private _permits;
 
     constructor(
         address owner_,
@@ -23,13 +24,24 @@ contract ApprovalNFT is ERC721, IERC721Receiver, Ownable, Nonces, ReentrancyGuar
     /**
      * @dev PermitSingle will have to be converted to PermitBatch in the frontend
      * @notice Mint an NFT and set the permit for the NFT
-     * @param permit The permissions for a batch of tokens of the NFT holder
      * @param to The address of the NFT holder
+     * @param permit The permissions for a batch of tokens of the NFT holder
+     * @param signature The signature of the permit
      */
     function mintAllowanceNFT(
-        IAllowanceTransfer.PermitBatch memory permit,
-        address to
-    ) external { }
+        address to,
+        IAllowanceTransfer.PermitBatch calldata permit, // assumes all relevant tokens will be found out before
+        bytes calldata signature
+    ) external {
+        address sender = _msgSender();
+        // permit this contract using permit 2
+        // TODO: ensure in frontend spender is this contract
+        _PERMIT_2.permit(sender, permit, signature);
+
+        uint256 nextId = totalSupply();
+        _mint(to, nextId);
+        _permits[nextId] = permit.details;
+    }
 
     /**
      * @dev PermitSingle will have to be converted to PermitBatch in the frontend
@@ -38,13 +50,34 @@ contract ApprovalNFT is ERC721, IERC721Receiver, Ownable, Nonces, ReentrancyGuar
      * @param to The address of the NFT holder
      */
     function safeMintAllowanceNFT(
-        IAllowanceTransfer.PermitBatch memory permit,
-        address to
-    ) external { }
+        address to,
+        IAllowanceTransfer.PermitBatch calldata permit, // assumes all relevant tokens will be found out before
+        bytes calldata signature
+    ) external {
+        address sender = _msgSender();
+        // permit this contract using permit 2
+        // TODO: ensure in frontend spender is this contract
+        _PERMIT_2.permit(sender, permit, signature);
+
+        uint256 nextId = totalSupply();
+        _safeMint(to, nextId);
+        _permits[nextId] = permit.details;
+    }
 
     /* ------------------------------------------------------------------ */
     /* Transfer Funds Functions                                           */
     /* ------------------------------------------------------------------ */
+    /**
+     
+     */
+    function transferFunds() external {
+        // check if sender has one or more NFTs, revert if not
+        // transfer NFT back to this contract
+        // grab associated permit
+        // transfer funds
+        // destroy NFT & delete permit
+    }
+
     /**
      */
     function onERC721Received(
