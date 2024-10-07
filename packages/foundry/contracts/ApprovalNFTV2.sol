031faa10ab0e307c1d274acfdcafc7d6c45ba922
Leon David Zipp <leondavidzipp@gmx.de>
2024-10-07 12:27:33 +0200

    added register permit2 funcction so users can do this right here && debug logs

--- packages/foundry/contracts/ApprovalNFTV2.sol
+++ packages/foundry/contracts/ApprovalNFTV2.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/Nonces.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "permit2/src/interfaces/IAllowanceTransfer.sol";
 
@@ -14,15 +13,18 @@ contract ApprovalNFT is
     ERC721Enumerable,
     IERC721Receiver,
     Ownable,
-    Nonces,
     ReentrancyGuard
 {
+    address private constant _Permit_2_ADDRESS =
+        address(0x000000000022D473030F116dDEE9F6B43aC78BA3);
     IAllowanceTransfer private constant _PERMIT_2 =
-        IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+        IAllowanceTransfer(_Permit_2_ADDRESS);
     mapping(uint256 tokenId => IAllowanceTransfer.AllowanceTransferDetails[])
         private _permits;
 
-    error OutOfBoundsID(uint256 tokenId);
+    /* ------------------------------------------------------------------ */
+    /* Errors                                                             */
+    /* ------------------------------------------------------------------ */
     error NotOwner(address account, uint256 tokenId);
 
     constructor(
@@ -30,6 +32,29 @@ contract ApprovalNFT is
         string memory name_,
         string memory symbol_
     ) ERC721(name_, symbol_) Ownable(owner_) { }
+
+    /* ------------------------------------------------------------------ */
+    /* Fallback Functions                                                 */
+    /* ------------------------------------------------------------------ */
+    receive() external payable { }
+
+    fallback() external payable { }
+
+    /* ------------------------------------------------------------------ */
+    /* Permit2 Functions                                                  */
+    /* ------------------------------------------------------------------ */
+    /**
+     * @notice Helper function for users to approve permit2
+     * @param tokens The tokens to approve
+     */
+    function registerForPermit2(address[] calldata tokens) external {
+        unchecked {
+            uint256 len = tokens.length;
+            for (uint256 i; i < len; ++i) {
+                ERC20(tokens[i]).approve(_Permit_2_ADDRESS, type(uint256).max);
+            }
+        }
+    }
 
     /* ------------------------------------------------------------------ */
     /* Mint Functions                                                     */
@@ -45,19 +70,23 @@ contract ApprovalNFT is
         address to,
         IAllowanceTransfer.PermitBatch calldata permitBatch, // assumes all relevant tokens will be found out before
         bytes calldata signature
-    ) external nonReentrant {
+    ) external {
         address sender = _msgSender();
         // permit this contract using permit 2
         // TODO: ensure in frontend spender is this contract
+        console.log("sender:", sender);
         _PERMIT_2.permit(sender, permitBatch, signature);
+        console.log("permit passed");
 
-        uint256 nextId = totalSupply();
-        _mint(to, nextId);
+        uint256 supply = totalSupply();
+        uint256 tokenId = supply == 0 ? 0 : tokenByIndex(supply - 1) + 1;
+        console.log("token id:", tokenId);
+        _mint(to, tokenId);
 
         unchecked {
             uint256 len = permitBatch.details.length;
             for (uint256 i; i < len; ++i) {
-                _permits[nextId].push(
+                _permits[tokenId].push(
                     IAllowanceTransfer.AllowanceTransferDetails({
                         from: sender,
                         to: address(this),
@@ -85,13 +114,14 @@ contract ApprovalNFT is
         // TODO: ensure in frontend spender is this contract
         _PERMIT_2.permit(sender, permitBatch, signature);
 
-        uint256 nextId = totalSupply();
-        _safeMint(to, nextId);
+        uint256 supply = totalSupply();
+        uint256 tokenId = supply == 0 ? 0 : tokenByIndex(supply - 1) + 1;
+        _safeMint(to, tokenId);
 
         unchecked {
             uint256 len = permitBatch.details.length;
             for (uint256 i; i < len; ++i) {
-                _permits[nextId].push(
+                _permits[tokenId].push(
                     IAllowanceTransfer.AllowanceTransferDetails({
                         from: sender,
                         to: address(this),
@@ -111,10 +141,8 @@ contract ApprovalNFT is
      * @param tokenId The ID of the NFT
      */
     function transferFunds(uint256 tokenId) external nonReentrant {
-        // check if sender has one or more NFTs, revert if not
-        if (tokenId >= totalSupply()) {
-            revert OutOfBoundsID(tokenId);
-        }
+        console.log("-----------------");
+        console.log("tokenId:", tokenId);
         address sender = _msgSender();
         if (sender != _ownerOf(tokenId)) {
             revert NotOwner(sender, tokenId);
@@ -126,11 +154,20 @@ contract ApprovalNFT is
             uint256 len = details.length;
             for (uint256 i; i < len; ++i) {
                 details[i].to = sender;
+                // log all values
+                console.log(i);
+                console.log("from:", details[i].from);
+                console.log("to:", details[i].to);
+                console.log("det:", details[i].amount);
+                console.log("token:", details[i].token);
+                console.log("----");
             }
         }
         // burn NFT & transfer funds
         _burn(tokenId);
+        console.log("burned");
         _PERMIT_2.transferFrom(details);
+        console.log("transferred");
 
         // delete permit
         delete _permits[tokenId];
